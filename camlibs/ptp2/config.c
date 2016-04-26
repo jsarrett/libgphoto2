a2548611110c024c4247445e6285182c12470a65
James Sarrett <jsarrett@gmail.com>
2016-04-26 14:47:54 -0700

    fix type for autofocus and capture settings

--- camlibs/ptp2/config.c
+++ camlibs/ptp2/config.c
@@ -6721,8 +6721,8 @@ static struct submenu camera_actions_menu[] = {
 	{ N_("Synchronize camera date and time with PC"),"syncdatetimeutc", PTP_DPC_CANON_EOS_UTCTime, PTP_VENDOR_CANON, PTP_DTC_UINT32, _get_Canon_SyncTime, _put_Canon_SyncTime },
 	{ N_("Synchronize camera date and time with PC"),"syncdatetime", PTP_DPC_CANON_EOS_CameraTime, PTP_VENDOR_CANON, PTP_DTC_UINT32, _get_Canon_SyncTime, _put_Canon_SyncTime },
 
-	{ N_("Auto-Focus"),                      "autofocus",           PTP_DPC_SONY_AutoFocus,  PTP_VENDOR_SONY,   0,          _get_Sony_Autofocus,            _put_Sony_Autofocus },
-	{ N_("Capture"),                        "capture",              PTP_DPC_SONY_Capture, PTP_VENDOR_SONY,   0,             _get_Sony_Capture,              _put_Sony_Capture },
+	{ N_("Auto-Focus"),                     "autofocus",            PTP_DPC_SONY_AutoFocus, PTP_VENDOR_SONY, PTP_DTC_UINT16, _get_Sony_Autofocus,           _put_Sony_Autofocus },
+	{ N_("Capture"),                        "capture",              PTP_DPC_SONY_Capture, PTP_VENDOR_SONY, PTP_DTC_UINT16,   _get_Sony_Capture,             _put_Sony_Capture },
 	{ N_("Power Down"),                     "powerdown",        0,  0,                  PTP_OC_PowerDown,                   _get_PowerDown,                 _put_PowerDown },
 	{ N_("Focus Lock"),                     "focuslock",        0,  PTP_VENDOR_CANON,   PTP_OC_CANON_FocusLock,             _get_Canon_FocusLock,           _put_Canon_FocusLock },
 	{ N_("Bulb Mode"),                      "bulb",             0xd2c1,  PTP_VENDOR_SONY,   0,                              _get_Sony_Bulb,                 _put_Sony_Bulb },
